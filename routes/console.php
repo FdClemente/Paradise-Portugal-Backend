63eb1c588b4bc53c1deb171de7ce8d3300ac76eb
Filipe Clemente <dev@filipeclemente.pt>
2025-06-24 11:19:42 +0100

    Add daily house import task and adjust scheduler (v1.0.0)
       - Removed unused code block related to reservation:start.
       - Introduced a new scheduled command 'import:houses' to run at midnight daily.
       - Adjusted the scheduler to run the new import:houses command and keep the existing reservation:send-reminder command at 10:00 AM daily.

--- routes/console.php
+++ routes/console.php
@@ -4,16 +4,13 @@ use App\Console\Commands\Reservation\SetReservationAsInProgressCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
-
-
-
 /*Schedule::command('reservation:start')
     ->daily()
     ->at('07:00');*/
 
+Schedule::command('import:houses')
+    ->daily()
+    ->at('00:00');
 Schedule::command('reservation:send-reminder')
     ->daily()
     ->at('10:00');
